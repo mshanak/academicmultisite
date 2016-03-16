10a748a95c0cb090ade6e2cbad02d45b9eaa76bc
mahdi_a <mahdi_a@ppu.edu>
2016-03-16 13:29:34 +0200

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UsersController extends Controller
+{
+    
+    
+    
+    public function store(){
+        
+        $u = new \App\User();
+        $u->name="ibrahim";
+        $u->email="ibrahim@ggg.com";
+        
+        $u->save();
+        
+        
+    }
+    
+    
+}
